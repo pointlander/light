c6330e0e40f04f0fefd877520f7a8b378f6d4890
Andrew Snodgrass <pointlander@gmail.com>
2025-04-04 08:10:44 -0500

    first commit

---
+++ main.go
@@ -0,0 +1,52 @@
+// Copyright 2025 The Light Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+// Prompt is a llm prompt
+type Prompt struct {
+	Model  string `json:"model"`
+	Prompt string `json:"prompt"`
+}
+
+// Query submits a query to the llm
+func Query(query string) string {
+	prompt := Prompt{
+		Model:  "llama3.2",
+		Prompt: query,
+	}
+	data, err := json.Marshal(prompt)
+	if err != nil {
+		panic(err)
+	}
+	buffer := bytes.NewBuffer(data)
+	response, err := http.Post("http://10.0.0.54:11434/api/generate", "application/json", buffer)
+	if err != nil {
+		panic(err)
+	}
+	reader, answer := bufio.NewReader(response.Body), ""
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+		data := map[string]interface{}{}
+		err = json.Unmarshal([]byte(line), &data)
+		text := data["response"].(string)
+		answer += text
+	}
+	return answer
+}
+
+func main() {
+	fmt.Println(Query("hello world!"))
+}
